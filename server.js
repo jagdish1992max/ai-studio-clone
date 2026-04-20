e8bb73a6abb64b1253b0847af683d53a71c083f8
Jd Butiya <jagdishjd6@gmail.com>
2026-04-20 22:03:18 +0530

    server.js

--- server.js
+++ server.js
@@ -3,16 +3,13 @@ const path = require("path");
 
 const app = express();
 
-// static frontend
 app.use(express.static(path.join(__dirname, "public")));
 
-// homepage
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Render port fix
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log("Server running");
 });
