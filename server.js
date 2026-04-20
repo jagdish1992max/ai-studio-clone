766e7dc26073a697b4bc3ab1694f68a87faaf79b
Jd Butiya <jagdishjd6@gmail.com>
2026-04-20 22:42:12 +0530

    server.js

--- server.js
+++ server.js
@@ -18,16 +18,4 @@ app.get("/", (req, res) => {
   res.send("🚀 Website is working");
 });
 
-const express = require("express");
-const path = require("path");
 
-const app = express();
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT);
